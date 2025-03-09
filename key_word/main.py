7ecde6981f718ca7b6d3bc550d39a1a91b83a91a
artyomkorchagin <steamartem@gmail.com>
2025-03-09 20:36:30 +0300

    improved code readability

--- key_word/main.py
+++ key_word/main.py
@@ -8,15 +8,18 @@ if __name__ == '__main__':
     print(matrix)
     encoded = ''
     cursor = 0
+
     message = MESSAGE + (X * Y - len(MESSAGE)) * ' '
+
     for i in range(X):
         for J in range(Y):
             matrix[KEYWORD[i]].append(message[cursor])
             cursor += 1
+
     for i in range(Y):
         for j in range(X):
             encoded += matrix[sorted(KEYWORD)[j]][i]
+
     print(encoded)
-    print(sorted(matrix))
 
 
