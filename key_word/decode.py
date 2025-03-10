2d6807ccbfda44b76f18c37da6b4fce02b91b2bd
artyomkorchagin <steamartem@gmail.com>
2025-03-10 12:11:51 +0300

    decoded key word

--- key_word/decode.py
+++ key_word/decode.py
@@ -1,6 +1,6 @@
 # This one doesn't work if characters in KEYWORD are not unique
-MESSAGE = 'ЧЕЗВ-ЛПА-ЫСПЬА-ЖВТА-РВЕ-РРОК- -ТЕОТ'
-KEYWORDS = ['ПОВОРОТ', 'КАТОРГА', 'ПИЛОТ']
+MESSAGE = 'ЧЕЗВ-ЛПА-ЫСПЬА-ЖВТА-РВЕ-РРОК--ТЕОТЕ'
+KEYWORDS = ['ПОВОРОТ', 'КАТОРГБ', 'ПИЛОТ'] # CHANGED КАТОРГА TO КАТОРГБ TO PREVENT COLLISION
 Y = 5
 if __name__ == '__main__':
     for keyword in KEYWORDS:
@@ -10,13 +10,13 @@ if __name__ == '__main__':
         x = len(keyword)
         message = MESSAGE + (x * Y - len(MESSAGE)) * ' '
 
-        for i in range(x):
-            for J in range(Y):
+        for j in range(Y):
+            for i in range(x):
                 matrix[sorted(keyword)[i]].append(message[cursor])
                 cursor += 1
 
-        for i in range(Y):
-            for j in range(x):
-                decoded += matrix[keyword[j]][i]
+        for i in range(x):
+            for j in range(Y):
+                decoded += matrix[keyword[i]][j]
 
         print(decoded)
